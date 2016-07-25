6e898c4d012a094290f11ec651ccca46d9e695ff
Steven Taylor <steven@vubeology.com>
2016-07-25 15:04:01 -0500

    Initital Commit. Sample add for getting an authed cookie redirect for a partner application

---
+++ index.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var partners = require('./partner-requests');
+
+var app = express();
+app.use(bodyParser());
+
+app.get('/', function(req, res){
+  var html = '<form action="/" method="post">' +
+							  '<br>' +
+								'<br>' +
+								'Enter username: ' +
+								'<input type="text" name="userName" />' +
+								'<br>' +
+								'<button type="submit">Submit</button>' +
+							'</form>';
+               
+  res.send(html);
+});
+
+// This route receives the posted form.
+// Will populate cookie on requester's client, and redirect to embedded stream.
+app.post('/', function(req, res){
+	var userName = req.body.userName;
+	partners.createUserAuthAndGetCookieUrl(userName, function(err, redirectUrl){
+		if(err || !redirectUrl) return res.status(500).send();
+		res.redirect(redirectUrl);
+	});
+});
+
+
+app.listen(3000);
---
+++ partner-requests.js
@@ -0,0 +1,80 @@
+var request = require('request');
+var Requests = module.exports = {};
+
+var config = {
+	apiHostname: 'https://partners-ids3.dev.caster.tv',
+	clientSlug: 'discord-dev',
+	clientId: 'clientId',          // TODO Fill in!
+	clientSecret: 'clientSecret',  // TODO Fill in!
+}
+
+var redirectUrl = 'https://ids3.dev.caster.tv/stream-embed/starcitizen/app/web/fullframe'
+
+Requests.createUser = function(username, cb){
+	var url = config.apiHostname + '/api/v1/' + config.clientSlug + '/users';
+	request({
+		method: 'POST',
+		url: url,
+		json: {
+			username: username
+		},
+		timeout: 1000,
+		auth: {
+			user: config.clientId,
+			pass: config.clientSecret
+		}
+	}, function (err, resp, body) {
+		if (err) {
+			console.log('create-user-failed', err);
+			return cb(err);
+		}
+		// 409 status means user already created
+		if (resp.statusCode >= 400 && resp.statusCode != 409) {
+			console.log('create-user-failed:statusCode', resp.statusCode);
+			return cb();
+		}
+		return cb(null, body);
+	});
+}
+
+
+Requests.authCookieToken = function (username, cb){
+	request({
+		method: 'POST',
+		url: config.apiHostname + '/api/v1/' + config.clientSlug + '/users/' + username + '/login-token',
+		json: {
+			username: username
+		},
+		timeout: 1000,
+		auth: {
+			user: config.clientId,
+			pass: config.clientSecret
+		}
+	}, function (err, resp, body) {
+		if (err) {
+			console.log('auth-cookie-failed', err);
+			return cb(err);
+		}
+		if (resp.statusCode >= 400) {
+			console.log('auth-cookie-failed:statusCode', resp.statusCode);
+			return cb();
+		}
+		return cb(null, body.token);
+	});
+}
+
+var authCookieTokenUrl = function(username, token){
+	var url = config.apiHostname + '/api/v1/' + config.clientSlug + '/users/' + username + '/login-token/' + token;
+	return url+'?url='+redirectUrl;
+}
+
+
+Requests.createUserAuthAndGetCookieUrl = function(username, cb){
+	Requests.createUser(username, function(err, result){
+		if(err) return cb(err);
+		Requests.authCookieToken(username, function(err, token){
+			if(err || !token) return cb(err);
+			cb(null, authCookieTokenUrl(username, token));
+		});
+	});
+}
